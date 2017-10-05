935f94bb0de012b9539b63e7b14ca48bfa26d630
David Charte <fdavidcl@protonmail.com>
2017-10-05 20:03:48 +0200

    Fix bug in hold out partitioning

--- docs/partition_mld.r
+++ docs/partition_mld.r
@@ -20,17 +20,17 @@ ALT_SEED <- 31638452
 # (mld : "mldr", f : (mld, k, seed) -> mldr.folds) -> list() : named list with a mldr for each file to be written
 VALIDATION = list (
   hout = function(mld, f) {
-    folds <- f(mld, k = 10, seed = SEED)
+    folds <- f(mld, k = 10, seed = SEED, get.indices = T)
     list(
-      tra = Reduce(`+`, lapply(folds[1:6], function(x) x$train)),
-      tst = Reduce(`+`, lapply(folds[7:10], function(x) x$train))
+      tra = Reduce(c, lapply(folds[1:6], function(x) x$test)),
+      tst = Reduce(c, lapply(folds[7:10], function(x) x$test))
     )
   },
-  
+
   "2x5" = function(mld, f) {
-    folds <- c(f(mld, k = 5, seed = SEED),
-               f(mld, k = 5, seed = ALT_SEED))
-    
+    folds <- c(f(mld, k = 5, seed = SEED, get.indices = T),
+               f(mld, k = 5, seed = ALT_SEED, get.indices = T))
+
     # Name each fold and flatten the list of lists
     Reduce(c, lapply(1:length(folds), function(i) {
       l = list(folds[[i]]$train, folds[[i]]$test)
@@ -38,10 +38,10 @@ VALIDATION = list (
       l
     }))
   },
-  
+
   "1x10" = function(mld, f) {
-    folds <- f(mld, k = 10, seed = SEED)
-    
+    folds <- f(mld, k = 10, seed = SEED, get.indices = T)
+
     # Name each fold and flatten the list of lists
     Reduce(c, lapply(1:length(folds), function(i) {
       l = list(folds[[i]]$train, folds[[i]]$test)
@@ -61,18 +61,20 @@ should_sparse <- function(mld) {
 
 partition <- function(mld, name) {
   sparse = should_sparse(mld)
-  
+
   for (s in 1:length(STRATEGIES)) {
     for (v in 1:length(VALIDATION)) {
       fold_list <- VALIDATION[[v]](mld, STRATEGIES[[s]])
-      
+
       for (g in names(fold_list)) {
         basename <- paste(name, names(STRATEGIES)[s], names(VALIDATION)[v], g, sep = "-")
-        
+
+        fold_object <- mldr::mldr_from_dataframe(mld$dataset[fold_list[[g]],], labelIndices = mld$labels$index, attributes = mld$attributes, name = mld$name)
+
         for (f in FORMATS) {
-          mldr.datasets::write.mldr(fold_list[[g]], format = f, basename = basename, sparse = sparse)
+          mldr.datasets::write.mldr(fold_object, format = f, basename = basename, sparse = sparse)
         }
-        
+
         # RDS format
         saveRDS(fold_list[[g]], file = paste0(basename, ".rds"))
         cat("Exported", basename, "\n")
@@ -83,31 +85,31 @@ partition <- function(mld, name) {
 
 main <- function() {
   mldname <- commandArgs(trailingOnly = T)
-  
+
   if (length(mldname) == 1) {
     dataset <- get(mldname)
-    
+
     if (class(dataset) != "mldr") {
       # mldname <- check_n_load.mldr(mldname)
       mldname <- tryCatch(
         load(paste0("../mldr.datasets/additional-data/", mldname, ".rda")),
         error = function(e) {
-          
+
           mldname <- tryCatch(
             load(paste0("../mldr.datasets/data/", mldname, ".rda")),
             error = { cat("Couldn't find dataset :(") }
-          ) 
-          
+          )
+
           if (!is.null(mldname))
             dataset <- get(mldname)
         }
-      ) 
-      
+      )
+
       if (!is.null(mldname))
         dataset <- get(mldname)
-      
+
     }
-    
+
     if (class(dataset) == "mldr") {
       partition(dataset, mldname)
     } else {
---
+++ docs/write_all.r
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+require("mldr")
+require("mldr.datasets")
+
+sparseness <- function(mld) {
+  sum(mld$dataset == 0) / prod(dim(mld$dataset))
+}
+
+should_sparse <- function(mld) {
+  sparseness(mld) > 0.5
+}
+
+go <- function(dataset, name) {
+  mldr.datasets::write.mldr(
+    dataset[1:2],
+    basename = paste0("../multilabel-repo/", name),
+    sparse = should_sparse(dataset)
+  )
+}
+
+main <- function(mldname = commandArgs(trailingOnly = T)) {
+  if (length(mldname) == 1) {
+    dataset <- get(mldname)
+    
+    if (class(dataset) != "mldr") {
+      # mldname <- check_n_load.mldr(mldname)
+      mldname <- tryCatch(
+        load(paste0("../mldr.datasets/additional-data/", mldname, ".rda")),
+        error = function(e) {
+          mldname <- tryCatch(
+            load(paste0("../mldr.datasets/data/", mldname, ".rda")),
+            error = { cat("Couldn't find dataset :(") }
+          ) 
+          
+          if (!is.null(mldname))
+            dataset <- get(mldname)
+        }
+      ) 
+      
+      if (!is.null(mldname))
+        dataset <- get(mldname)
+    }
+    
+    if (class(dataset) == "mldr") {
+      go(dataset, mldname)
+    } else {
+      cat("Please specify a valid mld name\n")
+    }
+  } else {
+    cat("Usage: partition_mld.r mld_name\n")
+  }
+}
+
+.w <- function(x) strsplit(x, " ")[[1]]
+
+#datasets <- read.csv("https://gitlab.com/fdavidcl/mldr.datasets/raw/master/inst/extdata/mldrs.csv", header = T, stringsAsFactors = F)$Name
+#datasets <- setdiff(datasets, .w("eurlexdc_test eurlexdc_tra eurlexev_test eurlexev_tra eurlexsm_test eurlexsm_tra imdb nuswide_BoW nuswide_VLAD tmc2007 tmc2007_500"))
+#datasets <- .w("yahoo_arts yahoo_education yahoo_recreation yahoo_social yahoo_business yahoo_entertainment yahoo_reference yahoo_society yahoo_computers yahoo_health yahoo_science yeast")
+datasets <- .w("tmc2007")
+
+for (x in datasets) {
+  cat("Incoming:", x, "\n")
+  main(x)
+}
