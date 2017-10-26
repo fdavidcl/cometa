929ad70f582db78e79df6c15e16c880af2e83f3a
David Charte <fdavidcl@protonmail.com>
2017-10-26 02:12:54 +0200

    Improve plot generation

--- scripts/dataset_metadata.r
+++ scripts/dataset_metadata.r
@@ -13,6 +13,7 @@ if (class(dataset) == "mldr") {
     sum$name <- mldname
     sum$internal.name <- dataset$name
     sum$attributes <- data.frame(name = names(dataset$attributes), type = dataset$attributes, row.names = NULL)
+    sum$labels <- dataset$labels$index - 1 # label indices starting from 0
     sum$bibtex <- dataset$bibtex
 
     my_dir <- "public/json/"
@@ -23,12 +24,35 @@ if (class(dataset) == "mldr") {
     cat(toJSON(sum, pretty=T, auto_unbox = T))
     sink()
 
+    # Concurrence plot
+    selected <- order(dataset$labels$SCUMBLE, decreasing = T)
+    tryPlot <- function(filename, max_) {
+      selection <- dataset$labels$index[selected[1:min(max_, length(selected))]]
+      png(filename, width = 1024, height = 1024, pointsize = 15)
+      tryCatch({
+        plot(
+          dataset,
+          labelIndices = selection,
+          title = "",
+          color.function = colorspace::rainbow_hcl
+        )
+      },
+      error = function(...) {})
+      dev.off()
+    }
+    
     my_plot_dir <- "public/img/"
     if (!dir.exists(my_plot_dir))
         dir.create(my_plot_dir)
-    png(paste0(my_plot_dir, mldname, ".png"))
-    mldr:::plot.mldr(dataset)
-    dev.off()
+    filename <- paste0(my_plot_dir, mldname, ".png")
+    
+    amount <- 10
+    tryPlot(filename, amount)
+    while (!file.exists(filename) && amount < 100) {
+      amount <- amount + 10
+      cat("Trying to plot with", amount, "labels...\n")
+      tryPlot(filename, amount)
+    }
 } else {
     cat("Please specify a valid mld path\n")
 }
